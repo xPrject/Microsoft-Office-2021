a3f962ff8653e9785a46011edf188792978b0124
Xedric Antiola <xantiola@gmail.com>
2023-01-14 08:17:49 +0800

    Update UpdateKMS.bat

--- volume/UpdateKMS.bat
+++ volume/UpdateKMS.bat
@@ -1,9 +1,17 @@
+@echo off
+@color 0a
 cd /d %ProgramFiles%\Microsoft Office\Office16
 
+echo Installing Volume Activation Licences...
 for /f %x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
 
+echo Updating Key Management Server...
 cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /sethst:193.29.63.133
+
+echo Requesting for Activation...
 cscript ospp.vbs /unpkey:6F7TH >nul
 cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
-cscript ospp.vbs /sethst:193.29.63.133
 cscript ospp.vbs /act
+
+pause
